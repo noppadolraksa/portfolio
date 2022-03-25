f6c6a7cf180a26eb491497b346ae709bf2b1bdd4
noppadolraksa <90181120+noppadolraksa@users.noreply.github.com>
2022-03-26 02:44:03 +0700

    change uri in apolloclient

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
-  uri: "https://portfolio-noppadolraksa.vercel.app/api/graphql",
+  uri: "/api/graphql",
   cache: new InMemoryCache(),
 });
 
