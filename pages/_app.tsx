0d4829300e5be2314972e8a241bd2aea10eb3b68
noppadolraksa <90181120+noppadolraksa@users.noreply.github.com>
2022-03-26 04:10:47 +0700

    fixed theme with ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider, useTheme } from "next-themes";
 import { ApolloProvider } from "@apollo/client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import client from "src/lib/apollo";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
   return (
     <ApolloProvider client={client}>
       <ThemeProvider defaultTheme="system">
